10ac385c21e7a7d5ed758e50128b464f48a10355
Jan Rollmann <jan.rollmann@device-insight.com>
2022-09-15 18:54:06 +0200

    Update Ux4iot initialization test

--- src/__tests__/Ux4iot.test.ts
+++ src/__tests__/Ux4iot.test.ts
@@ -1,19 +1,26 @@
 import { Ux4iot } from '../library/base/Ux4iot';
+jest.mock('socket.io-client', () => {
+	return {
+		...jest.requireActual('socket.io-client'),
+		io: jest.fn().mockImplementation(() => ({
+			on: () => undefined,
+		})),
+	};
+});
 const getSessionIdMock = jest.fn();
 jest.mock('../library/base/Ux4iotApi', () => {
 	return {
 		Ux4iotApi: jest.fn().mockImplementation(() => ({
 			...jest.requireActual('../library/base/Ux4iotApi'),
 			getSessionId: getSessionIdMock,
+			getSocketURL: () => `fake`,
 		})),
 	};
 });
 
 describe('Ux4iot', () => {
-	it('initializes correctly', () => {
-		const ux4iot = new Ux4iot({
-			adminConnectionString: '',
-		});
+	it('should make exactly one call to getSessionId on initialization', () => {
+		new Ux4iot({ adminConnectionString: '' });
 
 		expect(getSessionIdMock).toBeCalledTimes(1);
 	});
